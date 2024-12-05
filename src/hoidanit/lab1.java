05ff6391e87fb24c81d8ba107dbafaec56b27742
nguyenthikieuhanh <lhanh111@gmail.com>
2024-12-05 14:31:28 +0700

    Bai 1 dung de lam quen java ne

---
+++ src/hoidanit/lab1.java
@@ -0,0 +1,14 @@
+package hoidanit;
+
+import java.util.Scanner;
+
+public class lab1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ten sinh vien");
+		String name = scanner.nextLine();
+		System.out.println("Diem trung binh");
+		int diem = scanner.nextInt();
+		System.out.println("Sinh vien " + name + " co diem trung binh la: " + diem);
+	}
+}
